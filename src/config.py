3c4d8fb731e181ec0b9ccb71913d08acd81393ca
Katarzyna Koloda <koloda.katarzyna@gmail.com>
2025-11-17 22:27:59 +0100

    update configs

--- src/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DUMP_FILE = "plwiki-latest-pages-articles.xml.bz2"
-DUMP_PATH = os.path.join("data", "raw", DUMP_FILE)
-DUMP_URL = f"https://dumps.wikimedia.org/plwiki/latest/{DUMP_FILE}"
-JSON_RAW = os.path.join("data", "raw", "plwiki_latest_articles.jsonl")
-PROCESSED_DIR = os.path.join("data", "processed")
